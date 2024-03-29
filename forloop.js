d75ebc4403d380f93b2c6b5c814dfe40696279ac
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-29 14:40:19 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,15 @@
+var a = 'hello world'
+
+for (var i = 0; i <=10; i++){
+    console.log(a)
+}
+
+var num = [2,3,4,5,6,7,8]
+
+var squaredArray = []
+
+for (var i = 0; i <=num.length; i++){
+    squaredArray.push(num[i]*num[i])
+}
+
+console.log(squaredArray);
