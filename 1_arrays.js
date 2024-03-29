1d9be976acd01e040b53e6c2511c3cf87d33ddbf
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-29 13:27:49 +0530

    Add files via upload

--- 1_arrays.js
+++ 1_arrays.js
@@ -8,3 +8,21 @@ console.log(arr[3])
 arr[1] = 'Bentley'
 console.log(arr[1])
 console.log('Length of the array is',arr.length)
+
+var arr2 = [12, 14, 56, 57]
+
+arr2.pop()
+console.log('popped arr2',arr2)
+
+arr2.push(100)
+
+console.log('pushed with 100 arr2',arr2)
+
+arr2.shift()
+
+console.log('shifted arr2',arr2)
+
+arr2.unshift(102)
+
+
+console.log('unshifted arr2',arr2)
