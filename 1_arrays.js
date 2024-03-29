1e27573075d145b796d989fa54a41b20ea94e943
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-29 13:10:33 +0530

    Add files via upload

---
+++ 1_arrays.js
@@ -0,0 +1,10 @@
+var arr = [12, 'Ferrari', true, 12.4]
+console.log(arr)
+
+var a=arr[1]
+console.log(a)
+console.log(arr[3])
+
+arr[1] = 'Bentley'
+console.log(arr[1])
+console.log('Length of the array is',arr.length)
