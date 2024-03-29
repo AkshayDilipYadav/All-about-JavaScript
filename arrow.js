46db8e428dbd207874bb235fa203bd000e89a128
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-29 16:09:04 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,16 @@
+let a = 2
+let test1 =  ()=>{
+    
+    console.log(a+2)
+}
+
+let test2 = ()=>{
+    console.log(a*2)
+}
+
+let test3 = (a , b)=>{
+    console.log(a+b)
+}
+test1()
+test2(4)
+test3(2,6)
---
+++ letVSVar.js
@@ -0,0 +1,18 @@
+var a =20
+var a = 35
+
+let b = 30
+
+
+
+console.log(a)
+console.log(b)
+
+if (true){
+    let c = 40
+    console.log(c)
+}
+
+const d =100
+
+console.log(d)
