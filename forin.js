6e09500824e9b73a95ae719d1a102b350e02cf63
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-29 14:59:41 +0530

    Add files via upload

---
+++ forin.js
@@ -0,0 +1,15 @@
+var colors = {
+    primary : 'Blue',
+    secondary : 'Red',
+    tertiary : 'White'
+}
+
+for (var color in colors){
+    console.log(color + ' : '+colors[color])
+}
+
+var colorsArray = ['Orange', 'Green', 'yellow', 'Pink']
+
+for (var color in colorsArray){
+    console.log(color + ' : '+colorsArray[color])
+}
