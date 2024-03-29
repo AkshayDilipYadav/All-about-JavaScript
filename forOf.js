1847659cc5b9ef91a6c24f93ca3d0a956dce1da7
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-29 15:17:32 +0530

    Add files via upload

---
+++ forOf.js
@@ -0,0 +1,17 @@
+var scores = [60,90,80,75]
+
+for (var score of scores){
+    console.log(score)
+}
+
+let colors = ['red', 'white', 'green']
+
+for (var [index, color] of colors.entries()){
+    console.log(index+ ':' + color)
+}
+
+var str = 'Scaler'
+
+for (var c of str){
+    console.log(c)
+}
